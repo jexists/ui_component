9e09b75cda6cb3b7cf7caaa68742682ef4449518
jexists <jexists486@gmail.com>
2021-10-03 21:38:59 +0900

    context menu

--- context_menu/index.js
+++ context_menu/index.js
@@ -1,21 +1,49 @@
 
 const wrapper = document.querySelector('.wrapper');
-const items = document.querySelectorAll('.items');
+const items = document.querySelectorAll('.item');
 
-wrapper.addEventListener('click', function(e) {
-  const targetElem = e.target;
-  if (!targetElem.classList.contains('item')) return;
-  targetElem.classList.toggle('open');
-  items.forEach(function(elem) {
-    if (elem !== targetElem) elem.classList.remove('open');
-  })
-})
 
-// items.forEach(function(item) {
-//   items.addEventListener('click', function(e){
+// items.forEach(function (item) {
+//   items.addEventListener('click', function (e) {
 //     item.classList.toggle('open');
-//     items.forEach(function(elem) {
+//     items.forEach(function (elem) {
 //       if (elem !== itme) elem.classList.remove('open');
 //     })
 //   })
 // });
+
+// EventListener 최소화 하는것이 좋다.
+
+// wrapper.addEventListener('click', function(e) {
+//   const targetElem = e.target;
+//   e.stopPropagation();
+//   if (!targetElem.classList.contains('item')) return;
+//   targetElem.classList.toggle('open');
+//   items.forEach(function(elem) {
+//     if (elem !== targetElem) elem.classList.remove('open');
+//   })
+// });
+
+// 외부를 클릭
+// document.body.addEventListener('click', function(e) {
+//   if(e.target.classList.contains('context')) return;
+//   items.forEach(function(elem) {
+//     items.forEach()
+//   })
+// });
+
+document.body.addEventListener('click', function (e) {
+  const targetClassList = e.target.classList;
+  if (targetClassList.contains('context')) return;
+
+  if (targetClassList.contains('item')) {
+    targetClassList.toggle('open');
+    items.forEach(function (elem) {
+      if (elem !== e.target) elem.classList.remove('open');
+    });
+    return;
+  }
+  items.forEach(function (elem) {
+    elem.classList.remove('open');
+  });
+});
